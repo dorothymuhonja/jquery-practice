d287a194f9b930011f5bb4c421ea0521a0a783ae
dorothymuhonja <dorothymuhonja7@gmail.com>
2020-12-15 19:40:21 +0300

    add jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
-jQuery('h1').click(function(){
-  alert("This is a heading tag")
-});
+//  jQuery(document).ready(function () {
+//    jQuery('h1').click(function () {
+//      alert('This is a heading tag');
+//      alert('I told you this is a heading tag');
+//    });
 
-jQuery('p').click(function(){
-    alert("This is a paragraph")
-});
-jQuery('img').click(function(){
-    alert("This is an image")
-});
+//    jQuery('p').click(function () {
+//      alert('This is a paragraph');
+//    });
+//    jQuery('img').click(function () {
+//      alert('This is an image');
+//    });
+//  });
+
+  $(document).ready(function () {
+    // $('h1').click(function () {
+    //   alert('This is a heading tag');
+    //   alert('I told you this is a heading tag');
+    // });
+
+    // $('p').click(function () {
+    //   alert('This is a paragraph');
+    // });
+    // $('img').click(function () {
+    //   alert('This is an image');
+    // });
+    
+       $('.clickable').click(function() {
+        $('#walrus-showing').toggle();
+        $('#walrus-hidden').toggle();
+    });
+  });

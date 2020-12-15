0bd73863cd77efd7f776e3545bcaf88ded4c5156
dorothymuhonja <dorothymuhonja7@gmail.com>
2020-12-15 20:18:14 +0300

    add  js for buttons

--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,20 @@
         $('#walrus-showing').toggle();
         $('#walrus-hidden').toggle();
     });
+
+       $("button#green").click(function() {
+        $("body").removeClass();
+        $("body").addClass("green-background");
+    });
+
+        $('button#yellow').click(function () {
+        $('body').removeClass();
+        $('body').addClass('yellow-background');
+     });
+     
+        $('button#red').click(function () {
+        $('body').removeClass();
+        $('body').addClass('red-background');
+    });
+
   });
